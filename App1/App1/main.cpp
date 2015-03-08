9bdeddef33b8c0443bb6db415d222b697cab7243
ilovealyona <nickita.taranov@gmail.com>
2015-03-08 18:27:59 +0600

    commit 1.

---
+++ App1/App1/main.cpp
@@ -0,0 +1,10 @@
+#define _CRT_SECURE_NO_WARNINGS
+#include <iostream>
+using namespace std;
+
+int main() {
+	freopen("input.in", "r", stdin);
+	freopen("output.out", "w", stdout);
+	
+	return 0;
+}
