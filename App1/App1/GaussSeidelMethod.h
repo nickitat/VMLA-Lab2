17af8aff6cbf36e21ffe7af46311df805fd2b7d6
ilovealyona <nickita.taranov@gmail.com>
2015-04-07 22:53:05 +0600

    цкп

---
+++ App1/App1/GaussSeidelMethod.h
@@ -0,0 +1,131 @@
+#ifndef __GAUSS_SEIDEL_METHOD__
+#define __GAUSS_SEIDEL_METHOD__
+
+#include "calculation.h"
+
+#include <iostream>
+#include <vector>
+
+using namespace std;
+using namespace Garbage;
+
+#define db(x) cout << #x << '=' << (x) << "\n"
+
+const int maxn = 100;
+int C[maxn + 1][maxn + 1];
+
+void C_calculate() {
+	for (int n = 0; n <= maxn; ++n) {
+		C[n][0] = C[n][n] = 1;
+		for (int k = 1; k < n; ++k)
+			C[n][k] = C[n - 1][k - 1] + C[n - 1][k];
+	}
+}
+
+matrix get_inverse_matrix(int n) {
+	C_calculate();
+	matrix A(n, n);
+	for (int i = 0; i < n; ++i) {
+		A[i][0] = C[(n - 1) + i - 1][i - 1] * C[(n - 1)][i];
+		if ((n - 1 - i) & 1) {
+			A[i][0] *= -1;
+		}
+	}
+	for (int i = 0; i < n; ++i) {
+		for (int j = 0; j < n - 1; ++j) {
+			A[i][j + 1] = C[i + j][j] * C[i + j - 1][j - 1] * C[(n - 1) + i - 1][i + j] * C[(n - 1) + j][i + j] * i;
+			if ((i - j) & 1) {
+				A[i][j + 1] *= -1;
+			}
+		}
+	}
+	return A;
+}
+
+class GaussSeidelMethod {
+
+	typedef long double type;
+
+public:
+
+	static vector<type> solve(matrix& A, vector<type>& x, vector<type>& b, type omega, int& cntiter, int iteration_limit = 10000) {
+		int n = x.size();
+
+		matrix A_inv = get_inverse_matrix(n);
+		type A_inv_norm = norm(A_inv);
+		//db(A_inv_norm);
+
+		vector<type> x_prev = x, x_cur(n);
+		for (int it = 0; it < iteration_limit; ++it) {
+			for (int i = 0; i < n; ++i) {
+				type new_component = (1 - omega) * x_prev[i] + (omega)* b[i];
+				for (int j = 0; j < i; ++j) {
+					new_component -= A[i][j] * (omega)* x_cur[j];
+				}
+				for (int j = i + 1; j < n; ++j) {
+					new_component -= A[i][j] * (omega)* x_prev[j];
+				}
+				x_cur[i] = new_component / A[i][i];
+			}
+			if (converged(A, A_inv_norm, x_cur, b)) {
+				db(it);
+				break;
+			}
+			x_prev = x_cur;
+			cntiter++;
+		}
+		return x_cur;
+	}
+
+private:
+
+	static type norm(vector<type>& v) {
+		type norm = 0;
+		for (int i = 0; i < v.size(); ++i) {
+			norm += v[i] * v[i];
+		}
+		return sqrt(norm);
+	}
+
+	static type norm(matrix& A) {
+		int n = A.rows();
+		int m = A.columns();
+		type norm = 0;
+		for (int i = 0; i < n; ++i) {
+			for (int j = 0; j < m; ++j) {
+				norm += A[i][j] * A[i][j];
+			}
+		}
+		return sqrt(norm);
+	}
+
+	static bool converged(matrix& A, vector<type>& x, vector<type>& b, type eps = 1e-3) {
+		int n = A.rows();
+		matrix xx(n, 1);
+		for (int i = 0; i < n; ++i) {
+			xx[i][0] = x[i];
+		}
+		matrix ans = A * xx;
+		vector<type> err(n);
+		for (int i = 0; i < n; ++i) {
+			err[i] = ans[i][0] - b[i];
+		}
+		return norm(err) < eps;
+	}
+
+	static bool converged(matrix& A, type A_inv_norm, vector<type>& x_cur, vector<type>& b, type eps = 1e-5) {
+		int n = x_cur.size();
+		matrix xx(n, 1), bb(n, 1);
+		for (int i = 0; i < n; ++i) {
+			xx[i][0] = x_cur[i];
+			bb[i][0] = -b[i];
+		}
+		matrix R = (A * xx);
+		matrix P = R + bb;
+		type nnorm = norm(P);
+		//db(nnorm);
+		return nnorm * A_inv_norm < eps;
+	}
+};
+
+#endif/*__GAUSS_SEIDEL_METHOD__*/
--- App1/App1/main.cpp
+++ App1/App1/main.cpp
@@ -1,115 +1,23 @@
 #define _CRT_SECURE_NO_WARNINGS
 
-#include "calculation.h"
-using namespace Garbage;
-
-#define db(x) cout << #x << '=' << (x) << "\n"
+#include "GaussSeidelMethod.h"
 
 #include <algorithm>
 #include <ctime>
 #include <iomanip>
 #include <iostream>
 #include <numeric>
+
 using namespace std;
+using namespace Garbage;
 
-class GaussSeidelMethod {
-
-	typedef long double type;
-
-public:
-
-	static vector<type> solve(matrix& A, vector<type>& x, vector<type>& b, type omega, int& cntiter, int iteration_limit = 1000) {
-		int n = x.size();
-
-		type Cnorm;
-		matrix DL(n, n), U(n, n);
-		for (int i = 0; i < n; ++i) {
-			for (int j = 0; j < n; ++j) {
-				if (j <= i) {
-					DL[i][j] = A[i][j];
-				}
-				else {
-					U[i][j] = A[i][j];
-				}
-			}
-		}
-		matrix C = calculation::inverse_matrix::LU_method(DL);
-		C = C * U;
-		Cnorm = norm(C);
-
-		vector<type> x_prev = x, x_cur(n);
-		for (int it = 0; it < iteration_limit; ++it) {
-			for (int i = 0; i < n; ++i) {
-				type new_component = (1 - omega) * x_prev[i] + (omega)* b[i];
-				for (int j = 0; j < i; ++j) {
-					new_component -= A[i][j] * (omega)* x_cur[j];
-				}
-				for (int j = i + 1; j < n; ++j) {
-					new_component -= A[i][j] * (omega)* x_prev[j];
-				}
-				x_cur[i] = new_component / A[i][i];
-			}
-			if (converged(A, Cnorm, x_prev, x_cur)) {
-				db(it);
-				break;
-			}
-			x_prev = x_cur;
-			cntiter++;
-		}
-		return x_cur;
-	}
-
-private:
-
-	static type norm(vector<type>& v) {
-		type norm = 0;
-		for (int i = 0; i < v.size(); ++i) {
-			norm += v[i] * v[i];
-		}
-		return sqrt(norm);
-	}
-
-	static type norm(matrix& A) {
-		int n = A.rows();
-		type norm = 0;
-		for (int i = 0; i < n; ++i) {
-			for (int j = 0; j < n; ++j) {
-				norm += A[i][j] * A[i][j];
-			}
-		}
-		return sqrt(norm);
-	}
-
-	static bool converged(matrix& A, vector<type>& x, vector<type>& b, type eps = 1e-3) {
-		int n = A.rows();
-		matrix xx(n, 1);
-		for (int i = 0; i < n; ++i) {
-			xx[i][0] = x[i];
-		}
-		matrix ans = A * xx;
-		vector<type> err(n);
-		for (int i = 0; i < n; ++i) {
-			err[i] = ans[i][0] - b[i];
-		}
-		return norm(err) < eps;
-	}
-
-	static bool converged(matrix& A, type Cnorm, vector<type>& x_prev, vector<type>& x_cur, type eps = 1e-8) {
-		int n = x_prev.size();
-		vector<type> diff(n);
-		for (int i = 0; i < n; ++i) {
-			diff[i] = x_prev[i] - x_cur[i];
-		}
-		type errnorm = norm(diff);
-		return abs((errnorm * Cnorm / (1 - Cnorm))) < eps;
-	}
-};
+#define db(x) cout << #x << '=' << (x) << "\n"
 
 int main() {
 	freopen("output.out", "w", stdout);
 	srand(time(nullptr));
 
-	int n = 100;
+	int n = 3;
 	matrix A(n, n);
 
 	for (int i = 0; i < n; ++i) {
@@ -127,25 +35,24 @@ int main() {
 		b[i][0] = (i & 1) ? -1 : 1;
 	}
 
-	//matrix LUans = calculation::SLAE::LU_method(A, b);
-
 	b = A.transpos() * b;
 	A = A.transpos() * A;
-
-	//cout << "A:" << endl << A << << endl;
-	//cout << "b:" << endl << b << endl;
 
 	vector<long double> bb(n);
 	for (int i = 0; i < n; ++i) {
 		bb[i] = b[i][0];
 	}
 
-	int anscnt;
-	int cnt = 1000;
-	vector<long double> xi;
-	long double minerr = 1e15;
+	int cnt = 10000;
+	int anscnt = 10000;
+
 	matrix best(n, 1);
+	vector<long double> xi;
+	
+	long double minerr = 1e15;
+	long double ansomega = -1;
 	long double l = 0, r = 2;
+	
 	for (int i = 1; i < cnt; ++i) {
 		int itercnt = 0;
 		long double omega = l + (r - l) * long double(i) / long double(cnt);
@@ -160,6 +67,7 @@ int main() {
 			curerr += (err[j][0] - bb[j]) * (err[j][0] - bb[j]);
 		}
 		if (minerr > curerr) {
+			ansomega = omega;
 			minerr = curerr;
 			best = ans;
 			anscnt = itercnt;
@@ -167,6 +75,6 @@ int main() {
 	}
 
 	cout << anscnt << endl;
-	cout << fixed << setprecision(10) << minerr << endl;
+	cout << fixed << setprecision(10) << ansomega << " " << minerr << endl;
 	return 0;
 }
